0bcc74ae8cb7cf43abd6ae71e16ed3662db2c5d3
alexandriaware <alexandrialware@gmail.com>
2019-08-12 18:40:19 -0400

    Set up server connection string

---
+++ RecipeCard.Database/RecipeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using RecipeCard.Models;
+
+namespace RecipeCard.Database
+{
+    public class RecipeRepository
+    {
+        private string _recipeDb = ConfigurationManager.AppSettings["RecipeDb"];
+    }
+}
