9f9e7a9e3eb2946e8fa979729e1f3a7c9ad5a31a
alexandriaware <alexandrialware@gmail.com>
2019-08-12 18:20:10 -0400

    Added Database and Model projects; created Recipe properties

---
+++ RecipeCard.Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeCard.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeIngredients { get; set; }
+        public string RecipeInstructions { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
